9c9f3143cc485f2ce1ef397cd23f7646649221dc
Florian Thomas <flo@florianthomas.net>
2013-08-15 11:20:22 +0200

    de is default, en foreign_language for now

--- lib/form_translation.rb
+++ lib/form_translation.rb
@@ -1,20 +1,20 @@
 require "form_translation/version"
 require "form_translation/for_model"
-require "simple_form/form_builder"
+require "form_translation/custom_form_builder"
 
 module FormTranslation
 
   mattr_accessor :default_language
-  @@default_language = :en
+  @@default_language = :de
 
   mattr_accessor :foreign_languages
-  @@foreign_languages = []
+  @@foreign_languages = [:en]
 
   def self.config
     yield self
   end
 
   def self.languages
-    [self.default_language].merge self.foreign_languages
+    (Array(self.default_language) + Array(self.foreign_languages)).uniq
   end
 end
