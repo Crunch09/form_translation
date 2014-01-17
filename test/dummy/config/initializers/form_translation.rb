c851ff7f622adf107752a32410ebf54f6922e787
Florian Thomas <flo@florianthomas.net>
2014-01-17 10:43:09 +0100

    removed initializer

--- test/dummy/config/initializers/form_translation.rb
+++ test/dummy/config/initializers/form_translation.rb
@@ -1,19 +1,3 @@
-require 'simple_form'
-
-module SimpleForm
-  module ActionViewExtensions
-    module FormHelper
-      alias :orig_simple_form_for :simple_form_for
-      def simple_form_for(object, *args, &block)
-        options = args.extract_options!
-        orig_simple_form_for(object,
-          *(args << options.merge(builder: FormTranslation::CustomFormBuilder)),
-          &block)
-      end
-    end
-  end
-end
-
 FormTranslation.config do |ft|
   ft.default_language = :en
   ft.foreign_languages = [:de]
