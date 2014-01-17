3a7fa871174b1df5d3fb825c09edfe5b501820a8
Florian Thomas <flo@florianthomas.net>
2014-01-17 11:20:27 +0100

    add the view helpers to ActionView

--- lib/form_translation/engine.rb
+++ lib/form_translation/engine.rb
@@ -1,4 +1,10 @@
 module FormTranslation
   class Engine < ::Rails::Engine
+    initializer 'form_translation' do |app|
+      ActiveSupport.on_load(:action_view) do
+        require 'form_translation/view_helper'
+        ActionView::Base.send(:include, FormTranslation::ViewHelper)
+      end
+    end
   end
 end
