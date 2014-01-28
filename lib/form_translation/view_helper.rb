05dbf8f326f83877a601ad13b399005a7c60d3c1
Florian Thomas <flo@florianthomas.net>
2014-01-28 10:39:16 +0100

    use `simple_nested_form_for` method instead of `simple_form_for` for nested_form wrapper

--- lib/form_translation/view_helper.rb
+++ lib/form_translation/view_helper.rb
@@ -17,7 +17,7 @@ module FormTranslation
     if defined?(FormTranslation::NestedCustomFormBuilder)
       def simple_translated_nested_form_for(object, *args, &block)
         options = args.extract_options!
-        simple_form_for(object,
+        simple_nested_form_for(object,
           *(args << options.merge(builder: FormTranslation::NestedCustomFormBuilder)),
           &block)
       end
